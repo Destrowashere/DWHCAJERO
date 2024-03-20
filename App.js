23a3f7a626d1f83d64e8b952449e4ff24124f6ef
agmethjimenez <agmeth.jimenez2005@gmail.com>
2024-03-20 12:57:45 -0500

    Vistas :vvvvvv

--- App.js
+++ App.js
@@ -1,6 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import Main from './src/components/main'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+const Stack = createNativeStackNavigator();
 export default function App() {
-  return  <Main />
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Main} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
